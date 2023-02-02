40a650fb541451fdcc80efd29610522f687f2c17
rafael.pereira <orafaeldev@gmail.com>
2023-02-01 21:12:02 -0300

    feat: adicao do context

---
+++ src/context/GlobalProvider.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useContext } from 'react'
+
+import { GlobalContext } from './GlobalContext'
+
+type GlobalProviderTypes = {
+    children: ReactNode
+}
+
+export const GlobalProvider = ({
+    children,
+}: GlobalProviderTypes) => {
+    const value = {}
+
+    return (
+        <GlobalContext.Provider value={value}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalState = () => {
+    const state = useContext(GlobalContext)
+
+    if (!state) {
+        throw new Error(
+            'useGlobalState must be within a useGlobalState provider'
+        )
+    }
+
+    return state
+}
