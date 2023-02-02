416506c76e142a9edf21bf1fddcdca86688f8241
rafael.pereira <orafaeldev@gmail.com>
2023-02-02 19:06:37 -0300

    feat: adicao de dados iniciais para popular os steps

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,50 @@
+import { StepsTypes } from './types'
+
 import * as S from './styles'
 
 const Sidebar = () => {
-    return <S.Wrapper></S.Wrapper>
+    const steps: StepsTypes[] = [
+        {
+            id: 1,
+            name: 'Step 1',
+            content: 'Your Info',
+            state: 'active',
+        },
+        {
+            id: 2,
+            name: 'Step 2',
+            content: 'Select Plan',
+            state: 'disabled',
+        },
+        {
+            id: 3,
+            name: 'Step 3',
+            content: 'Add-ons',
+            state: 'disabled',
+        },
+        {
+            id: 4,
+            name: 'Step 4',
+            content: 'Summary',
+            state: 'disabled',
+        },
+    ]
+
+    return (
+        <S.Wrapper>
+            {steps.map(({ id, name, content, state }) => (
+                <S.Step key={name}>
+                    <S.WrapperStepNumber state={state}>
+                        <S.Number>{id}</S.Number>
+                    </S.WrapperStepNumber>
+                    <S.StepContent>
+                        <S.StepName>{name}</S.StepName>
+                        <S.StepDescription>{content}</S.StepDescription>
+                    </S.StepContent>
+                </S.Step>
+            ))}
+        </S.Wrapper>
+    )
 }
 
 export default Sidebar
