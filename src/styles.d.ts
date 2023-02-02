9d90e98ffe5a31a0d249b147898453b697e064c7
rafael.pereira <orafaeldev@gmail.com>
2023-02-01 21:12:41 -0300

    feat: adicao de arquivos de declaracao de tipagens

---
+++ src/styles.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        bg: string
+        borderColor: string
+        denim: string
+        grey: string
+        lightBlue: string
+        lightGrey: string
+        orange: string
+        pink: string
+        purple: string
+        redErrors: string
+        skyBlue: string
+        veryLightGrey: string
+        white: string
+    }
+}
