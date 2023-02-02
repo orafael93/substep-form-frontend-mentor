67862f2c52f2adbe0fdb95f900b7addeecaf40d6
rafael.pereira <orafaeldev@gmail.com>
2023-02-02 19:06:16 -0300

    feat: adicao de estilizacao dos itens da sidebar na versao Desktop

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,7 +1,11 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 
 export const Wrapper = styled.div`
     ${({ theme }) => css`
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+
         width: 100%;
         height: 100%;
 
@@ -13,5 +17,71 @@ export const Wrapper = styled.div`
         background-repeat: no-repeat;
         background-position-y: bottom;
         background-size: contain;
+    `}
+`
+
+export const Step = styled.div`
+    display: flex;
+    gap: 1rem;
+
+    cursor: pointer;
+`
+
+type WrapperStepNumberTypes = {
+    state: 'active' | 'disabled'
+}
+
+const wrapperStepNumberModifier = {
+    active: (theme: DefaultTheme) => css`
+        background: ${theme.skyBlue};
+    `,
+    disabled: (theme: DefaultTheme) => css`
+        background: transparent;
+        border: 2px solid ${theme.white};
+        color: ${theme.white};
+    `,
+}
+
+export const WrapperStepNumber = styled.div<WrapperStepNumberTypes>`
+    ${({ theme, state }) => css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 33px;
+        height: 33px;
+
+        border-radius: 50%;
+
+        ${wrapperStepNumberModifier[state](theme)}
+    `}
+`
+
+export const Number = styled.div`
+    font-weight: bold;
+    font-size: 0.875rem;
+`
+
+export const StepContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    text-transform: uppercase;
+`
+
+export const StepName = styled.p`
+    ${({ theme }) => css`
+        text-transform: uppercase;
+        color: ${theme.lightBlue};
+    `}
+`
+
+export const StepDescription = styled.p`
+    ${({ theme }) => css`
+        color: ${theme.white};
+
+        font-size: 0.875rem;
+        font-weight: bold;
     `}
 `
