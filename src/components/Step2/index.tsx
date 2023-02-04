4ab160cfa5266821c368b5b1381c27a6fb89972e
rafael.pereira <orafaeldev@gmail.com>
2023-02-04 19:48:35 -0300

    feat: adicao de estrutura base e estilos do Step2

--- src/components/Step2/index.tsx
+++ src/components/Step2/index.tsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { MouseEvent, useState } from 'react'
+
+import ArcadeIcon from '../../assets/icon-arcade.svg'
+import AdvancedIcon from '../../assets/icon-advanced.svg'
+import ProIcon from '../../assets/icon-pro.svg'
+import Plan from '../Plan'
 
 import * as S from './styles'
 
+export type TypeOfPlans = 'Arcade' | 'Advanced' | 'Pro'
+
+const plans: {
+    icon: any
+    name: TypeOfPlans
+    price: number
+}[] = [
+    {
+        icon: <ArcadeIcon />,
+        name: 'Arcade',
+        price: 9,
+    },
+    {
+        icon: <AdvancedIcon />,
+        name: 'Advanced',
+        price: 12,
+    },
+    {
+        icon: <ProIcon />,
+        name: 'Pro',
+        price: 15,
+    },
+]
+
 const Step2 = () => {
-    return <S.Wrapper>Step2</S.Wrapper>
+    const [toggleChecked, setToggleChecked] = useState(false)
+    const [planSelected, setPlanSelected] = useState<TypeOfPlans>('Arcade')
+
+    const onToggle = (e: MouseEvent) => {
+        e.preventDefault()
+        setToggleChecked((oldState) => !oldState)
+    }
+
+    const onSelectPlan = (selectedPlan: TypeOfPlans) => {
+        setPlanSelected(selectedPlan)
+    }
+
+    return (
+        <S.Wrapper>
+            <S.StepTitle>Select your plan</S.StepTitle>
+            <S.StepDescription>
+                You have the option of monthly or yearly billing
+            </S.StepDescription>
+            <S.WrapperPlan>
+                {plans.map(({ name, price, icon }) => (
+                    <Plan
+                        key={name}
+                        planSelected={planSelected === name}
+                        icon={icon}
+                        name={name}
+                        price={toggleChecked ? price * 10 : price}
+                        typeOfPayment={toggleChecked ? 'yr' : 'mo'}
+                        onSelectPlan={onSelectPlan}
+                    />
+                ))}
+            </S.WrapperPlan>
+            <S.BillingFrequency>
+                <S.TypeOfPayment typeOfPayment={toggleChecked}>
+                    Monthly
+                </S.TypeOfPayment>
+                <S.WrapperToggle onClick={onToggle}>
+                    <S.Checkbox
+                        type="checkbox"
+                        checked={toggleChecked}
+                        readOnly
+                    />
+                    <S.WrapperPill>
+                        <S.Pill checked={toggleChecked} />
+                    </S.WrapperPill>
+                </S.WrapperToggle>
+                <S.TypeOfPayment typeOfPayment={!toggleChecked}>
+                    Yearly
+                </S.TypeOfPayment>
+            </S.BillingFrequency>
+            <S.WrapperButtons>
+                <S.BackButton>Go Back</S.BackButton>
+                <S.Button>Next Step</S.Button>
+            </S.WrapperButtons>
+        </S.Wrapper>
+    )
 }
 
 export default Step2
--- src/components/Step2/styles.ts
+++ src/components/Step2/styles.ts
@@ -1,3 +1,158 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    padding: 0 30px 0 8%;
+`
+export const StepTitle = styled.h2`
+    ${({ theme }) => css`
+        color: ${theme.denim};
+        font-size: 32px;
+        font-weight: bold;
+
+        @media (max-width: ${theme.breakpoints.tablet}px) {
+            font-size: 24px;
+        }
+    `}
+`
+
+export const StepDescription = styled.p`
+    ${({ theme }) => css`
+        color: ${theme.grey};
+        margin-top: 10px;
+        margin-bottom: 35px;
+        font-size: 16px;
+        font-weight: 400;
+
+        @media (max-width: ${theme.breakpoints.tablet}px) {
+            margin-bottom: 15px;
+        }
+    `}
+`
+
+export const WrapperPlan = styled.div`
+    ${({ theme }) => css`
+        display: flex;
+        gap: 18px;
+
+        @media (max-width: ${theme.breakpoints.tablet}px) {
+            flex-direction: column;
+        }
+    `}
+`
+
+export const BillingFrequency = styled.div`
+    ${({ theme }) => css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        gap: 24px;
+
+        padding: 10px 0;
+        border-radius: 8px;
+        margin-top: 32px;
+
+        background: ${theme.veryLightGrey};
+    `}
+`
+
+export const WrapperToggle = styled.label`
+    ${({ theme }) => css`
+        display: flex;
+
+        background: ${theme.denim};
+        border-radius: 18px;
+        width: 38px;
+        height: 20px;
+    `}
+`
+
+export const WrapperPill = styled.div`
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+`
+
+export type PillPropTypes = {
+    checked: boolean
+}
+
+export const Pill = styled.div<PillPropTypes>`
+    ${({ theme, checked }) => css`
+        width: 12px;
+        height: 12px;
+
+        background: ${theme.white};
+        border-radius: 50%;
+
+        transition: all.3s cubic-bezier(0.075, 0.82, 0.165, 1);
+        transform: ${`translateX(${checked ? 90 : -90}%)`};
+
+        cursor: pointer;
+    `}
+`
+
+export const Checkbox = styled.input`
+    display: none;
+`
+
+type TypeOfPaymentPropTypes = {
+    typeOfPayment: boolean
+}
+
+export const TypeOfPayment = styled.span<TypeOfPaymentPropTypes>`
+    ${({ theme, typeOfPayment }) => css`
+        color: ${typeOfPayment ? theme.grey : theme.denim};
+    `}
+`
+
+export const WrapperButtons = styled.div`
+    ${({ theme }) => css`
+        display: none;
+
+        @media (min-width: ${theme.breakpoints.tablet}px) {
+            display: flex;
+            justify-content: space-between;
+
+            margin-top: 100px;
+        }
+    `}
+`
+
+export const BackButton = styled.button`
+    ${({ theme }) => css`
+        background: transparent;
+
+        border: none;
+
+        color: ${theme.grey};
+
+        font-size: 16px;
+        font-weight: 500;
+        font-family: 'Ubuntu';
+
+        cursor: pointer;
+    `}
+`
+
+export const Button = styled.div`
+    ${({ theme }) => css`
+        background: ${theme.denim};
+
+        padding: 15px 25px;
+
+        color: ${theme.white};
+        border-radius: 8px;
+
+        transition: 0.2s;
+
+        &:hover {
+            background: #164a8a;
+            cursor: pointer;
+        }
+    `}
+`
