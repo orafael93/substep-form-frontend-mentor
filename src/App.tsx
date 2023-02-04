3c87959fbdf6e0800581eb167ba5a2f64ac9362c
rafael.pereira <orafaeldev@gmail.com>
2023-02-03 22:24:55 -0300

    feat: adicao do conteudo estatico no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import {
     GlobalWrapper,
     Wrapper,
+    WrapperContent,
+    WrapperNextStepMobile,
+    Button,
 } from './components/GlobalWrapper'
 import Sidebar from './components/Sidebar'
+import Step1 from './components/Step1'
 
 function App() {
     return (
         <GlobalWrapper>
             <Wrapper>
                 <Sidebar />
+                <WrapperContent>
+                    <Step1 />
+                </WrapperContent>
             </Wrapper>
+            <WrapperNextStepMobile>
+                <Button>Next Step</Button>
+            </WrapperNextStepMobile>
         </GlobalWrapper>
     )
 }
