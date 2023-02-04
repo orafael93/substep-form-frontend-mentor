450cd97aa47ce0a6ba33154eee44fb011d13b55d
rafael.pereira <orafaeldev@gmail.com>
2023-02-03 22:23:07 -0300

    feat: adicao do conteudo do Step1

--- src/components/Step1/index.tsx
+++ src/components/Step1/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
 import * as S from './styles'
 
 const Step1 = () => {
-    return <S.Wrapper>Step1</S.Wrapper>
+    return (
+        <S.Wrapper>
+            <S.StepTitle>Personal info</S.StepTitle>
+            <S.StepDescription>
+                Prease provide your name, email adress, and phone number
+            </S.StepDescription>
+            <S.WrapperInput>
+                <S.Label htmlFor="name">Name</S.Label>
+                <S.Input id="name" />
+            </S.WrapperInput>
+            <S.WrapperInput>
+                <S.Label htmlFor="name">Email Address</S.Label>
+                <S.Input type="email" id="name" />
+            </S.WrapperInput>
+            <S.WrapperInput>
+                <S.Label htmlFor="name">Phone number</S.Label>
+                <S.Input id="name" />
+            </S.WrapperInput>
+            <S.WrapperNextStep>
+                <S.Button>Next Step</S.Button>
+            </S.WrapperNextStep>
+        </S.Wrapper>
+    )
 }
 
 export default Step1
