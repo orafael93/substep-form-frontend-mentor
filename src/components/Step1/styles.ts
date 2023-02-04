ccbe4f11caeb055c6a1cb94419ff097b3865e9f6
rafael.pereira <orafaeldev@gmail.com>
2023-02-03 22:22:51 -0300

    feat: adicao do arquivo de testes do Step1

--- src/components/Step1/styles.ts
+++ src/components/Step1/styles.ts
@@ -1,3 +1,83 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div``
+
+export const WrapperInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 24px;
+`
+
+export const StepTitle = styled.h2`
+    ${({ theme }) => css`
+        color: ${theme.denim};
+        font-size: 32px;
+        font-weight: bold;
+    `}
+`
+
+export const StepDescription = styled.p`
+    ${({ theme }) => css`
+        color: ${theme.grey};
+        margin-top: 10px;
+        margin-bottom: 35px;
+        font-size: 16px;
+        font-weight: 400;
+    `}
+`
+
+export const Label = styled.label`
+    ${({ theme }) => css`
+        font-size: 14px;
+        font-weight: 400;
+        color: ${theme.denim};
+    `}
+`
+
+export const Input = styled.input`
+    ${({ theme }) => css`
+        padding: 15px;
+        background: ${theme.white};
+        border: 1px solid ${theme.borderColor};
+        border-radius: 8px;
+        margin-top: 8px;
+        font-size: 1rem;
+        font-weight: 500;
+        font-family: 'Ubuntu';
+
+        &:active {
+            border-color: ${theme.borderColor};
+        }
+    `}
+`
+
+export const WrapperNextStep = styled.div`
+    ${({ theme }) => css`
+        display: none;
+
+        @media (min-width: ${theme.breakpoints.tablet}px) {
+            display: flex;
+            justify-content: flex-end;
+
+            margin-top: 100px;
+        }
+    `}
+`
+
+export const Button = styled.div`
+    ${({ theme }) => css`
+        background: #164a8a;
+
+        padding: 15px 25px;
+
+        color: ${theme.white};
+        border-radius: 8px;
+
+        transition: 0.2s;
+
+        &:hover {
+            background: ${theme.denim};
+            cursor: pointer;
+        }
+    `}
+`
