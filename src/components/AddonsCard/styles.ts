0ad786675ad235acbbfb9d7284cd42c454e8129e
rafael.pereira <orafaeldev@gmail.com>
2023-02-21 19:44:21 -0300

    feat: adicao do arquivo de estilos do componente AddonsCard

---
+++ src/components/AddonsCard/styles.ts
@@ -0,0 +1,54 @@
+import styled, { css } from 'styled-components'
+
+type WrapperPropTypes = {
+    addonIsChecked?: boolean
+}
+
+export const Wrapper = styled.label<WrapperPropTypes>`
+    ${({ theme, addonIsChecked }) => css`
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+        padding: 16px 20px;
+        border-radius: 8px;
+
+        border: 1px solid ${theme.purple};
+
+        cursor: pointer;
+
+        background: ${addonIsChecked ? theme.veryLightGrey : theme.white};
+    `}
+`
+
+export const CheckboxInput = styled.input``
+
+export const WrapperService = styled.div``
+
+export const ServiceName = styled.p`
+    ${({ theme }) => css`
+        font-weight: 500;
+        font-size: 16px;
+        color: ${theme.denim};
+
+        margin-bottom: 7px;
+    `}
+`
+
+export const ServiceDescription = styled.p`
+    ${({ theme }) => css`
+        color: ${theme.grey};
+        font-weight: 400;
+        font-size: 15px;
+    `}
+`
+
+export const WrapperPrice = styled.div``
+
+export const Price = styled.p`
+    ${({ theme }) => css`
+        color: ${theme.purple};
+        font-size: 15px;
+        font-weight: 400;
+    `}
+`
