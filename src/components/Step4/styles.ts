8147e1d4bfaa370723af99d0f34e23062eb6bb2b
rafael.pereira <orafaeldev@gmail.com>
2023-03-06 21:15:48 -0300

    feat: adicao de estilos iniciais para o step4

--- src/components/Step4/styles.ts
+++ src/components/Step4/styles.ts
@@ -1,3 +1,24 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div``
+
+export const StepTitle = styled.h2`
+    ${({ theme }) => css`
+        color: ${theme.denim};
+        font-size: 32px;
+        font-weight: bold;
+
+        @media (max-width: ${theme.breakpoints.tablet}px) {
+            font-size: 24px;
+        }
+    `}
+`
+
+export const StepDescription = styled.p`
+    ${({ theme }) => css`
+        font-weight: 400;
+        font-size: 16px;
+        color: ${theme.grey};
+        margin-top: 10px;
+    `}
+`
