e0d31b1c39c8d26b59fe07bd9aec9be38a7820e2
rafael.pereira <orafaeldev@gmail.com>
2023-03-06 21:15:31 -0300

    feat: adicao de estrutura base para o step4

--- src/components/Step4/index.tsx
+++ src/components/Step4/index.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 import * as S from './styles'
 
 const Step4 = () => {
-    return <S.Wrapper>Step4</S.Wrapper>
+    return (
+        <S.Wrapper>
+            <S.StepTitle>Finishing up</S.StepTitle>
+            <S.StepDescription>
+                Double-check everything looks OK before confirming
+            </S.StepDescription>
+        </S.Wrapper>
+    )
 }
 
 export default Step4
