a371c9d40016a90fa8929f9e40f63fe9d6913e56
Rafael Pereira <orafaeldev@gmail.com>
2023-03-12 11:47:38 -0300

    feat: adicao do conteudo estatico dos addons

--- src/components/Step4/index.tsx
+++ src/components/Step4/index.tsx
@@ -18,7 +18,27 @@ const Step4 = () => {
                         <S.SelectedPrice>$9/mo</S.SelectedPrice>
                     </S.WrapperSelectedPrice>
                 </S.WrapperStepContent>
+                <S.WrapperAddons>
+                    <S.WrapperAddon>
+                        <S.AddonNameService>Online service</S.AddonNameService>
+                        <S.AddonPrice>+10/yr</S.AddonPrice>
+                    </S.WrapperAddon>
+                    <S.WrapperAddon>
+                        <S.AddonNameService>Larger storage</S.AddonNameService>
+                        <S.AddonPrice>+20/yr</S.AddonPrice>
+                    </S.WrapperAddon>
+                </S.WrapperAddons>
             </S.WrapperSelectedContent>
+
+            <S.WrapperTotalPrice>
+                <S.PriceText>Total (per year)</S.PriceText>
+                <S.PriceValue>$120/yr</S.PriceValue>
+            </S.WrapperTotalPrice>
+
+            <S.WrapperButtons>
+                <S.BackButton>Go Back</S.BackButton>
+                <S.Button>Confirm</S.Button>
+            </S.WrapperButtons>
         </S.Wrapper>
     )
 }
