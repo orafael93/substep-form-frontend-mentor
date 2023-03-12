031dbfb04720458eda5531f79dc901e319589518
Rafael Pereira <orafaeldev@gmail.com>
2023-03-12 12:26:28 -0300

    feat: adicao do arquivo de estilos do componente FinishedForm

---
+++ src/components/FinishedForm/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+    ${({ theme }) => css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        max-width: 80%;
+        height: 100%;
+
+        @media (max-width: ${theme.breakpoints.tablet}px) {
+            max-width: 90%;
+            padding: 50px 0;
+        }
+    `}
+`
+
+export const ThankYouText = styled.p`
+    ${({ theme }) => css`
+        font-weight: 700;
+        font-size: 32px;
+
+        color: ${theme.denim};
+
+        margin-top: 32px;
+    `}
+`
+
+export const ThankYouMessage = styled.p`
+    ${({ theme }) => css`
+        font-weight: 400;
+        font-size: large;
+
+        color: ${theme.grey};
+
+        text-align: center;
+
+        margin-top: 15px;
+        line-height: 25px;
+    `}
+`
