aa7f56a5a3ca6d1f9d43d27474c71287417b6c2e
Rafael Pereira <orafaeldev@gmail.com>
2023-04-11 08:41:06 -0300

    refactor: ajustes de estilos globais

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -7,27 +7,31 @@ import {
 
 const GlobalStyles: GlobalStyleComponent<{}, DefaultTheme> = createGlobalStyle`
     * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    
-    -webkit-font-smoothing: antialiased;
-    -mox-osx-font-smoothing: grayscale;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      
+      -webkit-font-smoothing: antialiased;
+      -mox-osx-font-smoothing: grayscale;
+      
+      &::before,
+      &::after {
+        box-sizing: inherit;
+      }
+  }
 
-    &::before,
-    &::after {
-      box-sizing: inherit;
-    }
-
-    ${({ theme }) => css`
-        html,
-        body {
-            width: 100vw;
-            height: 100vh;
-            background: ${theme.bg};
-            font-family: 'Ubuntu';
-        }
-    `}
+  ${({ theme }) => css`
+      html,
+      body {
+        width: 100%;
+        height: 100vh;
+        background: ${theme.bg};
+        font-family: 'Ubuntu';
+      }
+  `}
+      
+  #root {
+    height: 100%;
   }
 `
 
