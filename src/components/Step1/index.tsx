ae0d1bc6f6375f0b0bb65ddf34e892a19a995dfc
Rafael Pereira <orafaeldev@gmail.com>
2023-04-13 20:38:39 -0300

    feat: adicao de validacao para o Input

--- src/components/Step1/index.tsx
+++ src/components/Step1/index.tsx
@@ -1,6 +1,32 @@
+import { useState } from 'react'
+
+import { usePersonalInfo } from '../../utils/handlePersonalInfo'
+
 import * as S from './styles'
 
 const Step1 = () => {
+    const [canValidateFields, setCanValidateFields] = useState(false)
+
+    const [personalInfo, setPersonalInfo] = useState({
+        name: '',
+        email: '',
+        phone: '',
+    })
+
+    const unfilledFields = usePersonalInfo(personalInfo)
+    const canGoToNextStep = unfilledFields.length === 0
+
+    const inputNameHasError =
+        canValidateFields && unfilledFields.includes('name')
+    const inputEmailHasError =
+        canValidateFields && unfilledFields.includes('email')
+    const inputPhoneHasError =
+        canValidateFields && unfilledFields.includes('phone')
+
+    const onGoToNextStep = () => {
+        setCanValidateFields(true)
+    }
+
     return (
         <S.Wrapper>
             <S.StepTitle>Personal info</S.StepTitle>
@@ -8,23 +34,75 @@ const Step1 = () => {
                 Prease provide your name, email adress, and phone number
             </S.StepDescription>
             <S.WrapperInput>
-                <S.Label htmlFor="name">Name</S.Label>
-                <S.Input id="name" placeholder="e.g Stephen King" />
+                <S.WrapperLabelField>
+                    <S.Label htmlFor="name">Name</S.Label>
+                    {inputNameHasError && (
+                        <S.RequiredFieldText>
+                            This field is required.
+                        </S.RequiredFieldText>
+                    )}
+                </S.WrapperLabelField>
+                <S.Input
+                    id="name"
+                    placeholder="e.g Stephen King"
+                    value={personalInfo.name}
+                    onChange={(e) =>
+                        setPersonalInfo({
+                            ...personalInfo,
+                            name: e.target.value,
+                        })
+                    }
+                    error={inputNameHasError}
+                />
             </S.WrapperInput>
             <S.WrapperInput>
-                <S.Label htmlFor="name">Email Address</S.Label>
+                <S.WrapperLabelField>
+                    <S.Label htmlFor="name">Email Address</S.Label>
+                    {inputEmailHasError && (
+                        <S.RequiredFieldText>
+                            This field is required.
+                        </S.RequiredFieldText>
+                    )}
+                </S.WrapperLabelField>
                 <S.Input
                     type="email"
                     id="name"
                     placeholder="e.g stephenking@lorem.com"
+                    value={personalInfo.email}
+                    error={inputEmailHasError}
+                    onChange={(e) =>
+                        setPersonalInfo({
+                            ...personalInfo,
+                            email: e.target.value,
+                        })
+                    }
                 />
             </S.WrapperInput>
             <S.WrapperInput>
-                <S.Label htmlFor="name">Phone number</S.Label>
-                <S.Input id="name" placeholder="e.g +1 234 567 890" />
+                <S.WrapperLabelField>
+                    <S.Label htmlFor="name">Phone number</S.Label>
+
+                    {inputPhoneHasError && (
+                        <S.RequiredFieldText>
+                            This field is required.
+                        </S.RequiredFieldText>
+                    )}
+                </S.WrapperLabelField>
+                <S.Input
+                    id="name"
+                    placeholder="e.g +1 234 567 890"
+                    value={personalInfo.phone}
+                    error={inputPhoneHasError}
+                    onChange={(e) =>
+                        setPersonalInfo({
+                            ...personalInfo,
+                            phone: e.target.value,
+                        })
+                    }
+                />
             </S.WrapperInput>
             <S.WrapperNextStep>
-                <S.Button>Next Step</S.Button>
+                <S.Button onClick={onGoToNextStep}>Next Step</S.Button>
             </S.WrapperNextStep>
         </S.Wrapper>
     )
