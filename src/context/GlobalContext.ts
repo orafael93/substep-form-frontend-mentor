09f302c7b5d2c777e04c0acdacd2325709a1bff4
Rafael Pereira <orafaeldev@gmail.com>
2023-04-15 09:04:15 -0300

    feat: adicao da tipagem do contexto global

--- src/context/GlobalContext.ts
+++ src/context/GlobalContext.ts
@@ -1,3 +1,10 @@
 import { createContext } from 'react'
+import { StepsTypes } from '../components/Sidebar/types'
 
-export const GlobalContext = createContext({})
+export type GlobalContextStates = {
+    steps: StepsTypes[]
+}
+
+export const GlobalContext = createContext<GlobalContextStates>(
+    {} as GlobalContextStates
+)
