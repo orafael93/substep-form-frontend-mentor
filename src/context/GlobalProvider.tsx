5cc6bd1414a83a1748d4f0aa897a7da67afbf973
Rafael Pereira <orafaeldev@gmail.com>
2023-04-17 18:24:02 -0300

    feat: adicao dos modulos de cada step e o reducer dos steps

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -1,32 +1,104 @@
-import { ReactNode, useContext } from 'react'
+import { ReactNode, useReducer, useState } from 'react'
 
-import { GlobalContext } from './GlobalContext'
+import { Addons, GlobalContext, InfoTypes, Plan } from './GlobalContext'
 import { initDefaultSteps } from '../utils/initDefaultSteps'
+import { StepName, StepsTypes } from '../components/Sidebar/types'
 
 type GlobalProviderTypes = {
     children: ReactNode
 }
 
+export type StepsActions =
+    | {
+          type: 'activeStep'
+          payload: StepName
+      }
+    | {
+          type: 'completeStep'
+          payload: StepName
+      }
+
+const activeNextStep = (steps: StepsTypes[], stepIndexToActive: number) => {
+    const isLastStep = steps.length === stepIndexToActive
+
+    if (!isLastStep) {
+        steps[stepIndexToActive].state = 'active'
+    } else {
+        console.log('Último step')
+    }
+
+    return steps
+}
+
+const completeStep = (
+    steps: StepsTypes[],
+    stepNameToComplete: StepName
+): StepsTypes[] => {
+    const indexStepToComplete = steps.findIndex(
+        (step) => step.id === stepNameToComplete
+    )
+
+    const mappedSteps: StepsTypes[] = steps.map((step) => {
+        if (step.id === stepNameToComplete) {
+            return { ...step, state: 'completed' }
+        }
+
+        return step
+    })
+
+    return activeNextStep(mappedSteps, indexStepToComplete + 1)
+}
+
+const activeStep = (steps: StepsTypes[], stepNameToActive: StepName) => {
+    const nextStepToActive = steps.find((step) => step.id === stepNameToActive)
+
+    if (nextStepToActive?.state === 'disabled') {
+        return steps
+    }
+
+    const mappedSteps: StepsTypes[] = steps.map((step) => {
+        if (step.id === stepNameToActive) {
+            return { ...step, state: 'active' }
+        }
+
+        if (step.state === 'active') {
+            return { ...step, state: 'disabled' }
+        }
+
+        return step
+    })
+
+    return mappedSteps
+}
+
 export const GlobalProvider = ({ children }: GlobalProviderTypes) => {
-    const value = {
-        steps: initDefaultSteps(),
+    const initialSteps = initDefaultSteps()
+
+    const stepsReducer = (
+        state: StepsTypes[],
+        action: StepsActions
+    ): StepsTypes[] => {
+        switch (action.type) {
+            case 'activeStep':
+                return activeStep(state, action.payload)
+            case 'completeStep':
+                return completeStep(state, action.payload)
+            default:
+                return state
+        }
+    }
+
+    const states = {
+        infoModule: useState<InfoTypes>(),
+        planModule: useState<Plan>(),
+        addonsModule: useState<Addons[]>([]),
+        stepsModule: useReducer(stepsReducer, initialSteps),
+        completedFormModule: useState(false),
     }
 
     return (
-        <GlobalContext.Provider value={value}>
+        <GlobalContext.Provider value={states}>
             {children}
         </GlobalContext.Provider>
     )
-}
-
-export const useGlobalState = () => {
-    const state = useContext(GlobalContext)
-
-    if (!state) {
-        throw new Error(
-            'useGlobalState must be within a useGlobalState provider'
-        )
-    }
-
-    return state
 }
