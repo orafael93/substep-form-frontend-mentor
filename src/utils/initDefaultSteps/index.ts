946a27d80144d825cc96048b1e938aacc951b4ff
Rafael Pereira <orafaeldev@gmail.com>
2023-04-17 18:21:19 -0300

    refactor: adicao de slug nas chaves dos steps

--- src/utils/initDefaultSteps/index.ts
+++ src/utils/initDefaultSteps/index.ts
@@ -2,27 +2,31 @@ import { StepsTypes } from '../../components/Sidebar/types'
 
 export const initDefaultSteps = (): StepsTypes[] => [
     {
-        id: 1,
+        id: 'info',
         name: 'Step 1',
         content: 'Your Info',
         state: 'active',
+        slug: 1,
     },
     {
-        id: 2,
+        id: 'plan',
         name: 'Step 2',
         content: 'Select Plan',
         state: 'disabled',
+        slug: 2,
     },
     {
-        id: 3,
+        id: 'addons',
         name: 'Step 3',
         content: 'Add-ons',
         state: 'disabled',
+        slug: 3,
     },
     {
-        id: 4,
+        id: 'summary',
         name: 'Step 4',
         content: 'Summary',
         state: 'disabled',
+        slug: 4,
     },
 ]
