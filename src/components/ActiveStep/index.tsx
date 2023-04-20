a50856a54ead81f6ed21803cfe5527b977f80c37
Rafael Pereira <orafaeldev@gmail.com>
2023-04-20 17:59:06 -0300

    feat: adicao de validacao para retornar o componente correto a cada step

--- src/components/ActiveStep/index.tsx
+++ src/components/ActiveStep/index.tsx
@@ -3,22 +3,31 @@ import Step1 from '../Step1'
 import Step2 from '../Step2'
 import Step3 from '../Step3'
 import Step4 from '../Step4'
+import FinishedForm from '../FinishedForm'
+import { useCompletedForm } from '../../hooks/useCompletedForm'
 
 const ActiveStep = () => {
     const { steps } = useSteps()
+    const { completedForm } = useCompletedForm()
 
     const activeStep = steps.find((step) => step.state === 'active')
 
-    switch (activeStep?.id) {
-        case 'info':
-            return <Step1 />
-        case 'plan':
-            return <Step2 />
-        case 'addons':
-            return <Step3 />
-        default:
-            return <Step4 />
+    const handleActiveComponent = () => {
+        switch (activeStep?.id) {
+            case 'info':
+                return Step1
+            case 'plan':
+                return Step2
+            case 'addons':
+                return Step3
+            default:
+                return Step4
+        }
     }
+
+    const ActiveComponent = handleActiveComponent()
+
+    return completedForm ? <FinishedForm /> : <ActiveComponent />
 }
 
 export default ActiveStep
