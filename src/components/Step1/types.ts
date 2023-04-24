c800f2abb1907fa4f44622d6526bd7f112b3caa8
Rafael Pereira <orafaeldev@gmail.com>
2023-04-24 17:47:34 -0300

    feat: adicao de tipagem no componente Step1

--- src/components/Step1/types.ts
+++ src/components/Step1/types.ts
@@ -1,5 +1,9 @@
+import { Addons, AddonsTypes } from '../../context/GlobalContext'
+
 export type AddonsCardPropTypes = {
+    id: AddonsTypes
     serviceName: string
     serviceDescription: string
     servicePrice: string
+    onAddAddons: (params: Addons) => void
 }
