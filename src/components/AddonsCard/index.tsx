8f02e67e937216a6e1ad94a9ed6ef7356f219252
Rafael Pereira <orafaeldev@gmail.com>
2023-04-25 21:46:52 -0300

    feat: adicao de logica para adicionar addons

--- src/components/AddonsCard/index.tsx
+++ src/components/AddonsCard/index.tsx
@@ -1,20 +1,52 @@
 import { MouseEvent, useState } from 'react'
 
 import { AddonsCardPropTypes } from '../Step1/types'
+import { useAddons } from '../../hooks/useAddons'
+import { usePlan } from '../../hooks/usePlan'
 
 import * as S from './styles'
 
 const AddonsCard = ({
+    id,
     serviceName,
     serviceDescription,
     servicePrice,
+    onAddAddons,
 }: AddonsCardPropTypes) => {
-    const [checked, setChecked] = useState(false)
+    const { plan } = usePlan()
+    const { addons } = useAddons()
+    const addonAlreadyChecked = addons?.find(
+        (currentAddon) => currentAddon.type === id
+    )
+    const [checked, setChecked] = useState(!!addonAlreadyChecked || false)
 
     const handleCheck = (e: MouseEvent) => {
         e.preventDefault()
+
         setChecked((oldState) => !oldState)
+        onAddAddons({
+            type: id,
+            price: servicePrice,
+            frequency: plan?.frequency === 'month' ? 'month' : 'year',
+        })
     }
+
+    const handlePlanPriceAndFrequency = () => {
+        switch (plan?.frequency) {
+            case 'year':
+                return {
+                    price: Number(servicePrice) * 10,
+                    frequencyText: 'yr',
+                }
+            default:
+                return {
+                    price: servicePrice,
+                    frequencyText: 'mo',
+                }
+        }
+    }
+
+    const frequencyContent = handlePlanPriceAndFrequency()
 
     return (
         <S.Wrapper onClick={handleCheck} addonIsChecked={checked}>
@@ -26,7 +58,9 @@ const AddonsCard = ({
                 </S.ServiceDescription>
             </S.WrapperService>
             <S.WrapperPrice>
-                <S.Price>+${servicePrice}/mo</S.Price>
+                <S.Price>
+                    +${frequencyContent.price}/{frequencyContent.frequencyText}
+                </S.Price>
             </S.WrapperPrice>
         </S.Wrapper>
     )
