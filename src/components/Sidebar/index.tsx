2a20c5e049305c93d68c284c86f76b3a5510d90c
Rafael Pereira <orafaeldev@gmail.com>
2023-04-30 11:20:10 -0300

    feat: adicao de funcionalidade para ativar os steps

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,24 @@
-import { useGlobalContext } from '../../hooks/useGlobalContext'
+import { useCompletedForm } from '../../hooks/useCompletedForm'
+import { useSteps } from '../../hooks/useSteps'
 
 import * as S from './styles'
+import { StepName } from './types'
 
 const Sidebar = () => {
-    const { steps } = useGlobalContext()
+    const { steps, onActiveStep } = useSteps()
+    const { completedForm } = useCompletedForm()
+
+    const handleActiveStep = (idStepToActive: StepName) => {
+        if (completedForm) return
+        onActiveStep(idStepToActive)
+    }
 
     return (
         <S.Wrapper>
-            {steps.map(({ id, name, content, state }) => (
-                <S.Step key={name}>
+            {steps.map(({ id, name, content, state, slug }) => (
+                <S.Step key={name} onClick={() => handleActiveStep(id)}>
                     <S.WrapperStepNumber state={state}>
-                        <S.Number>{id}</S.Number>
+                        <S.Number>{slug}</S.Number>
                     </S.WrapperStepNumber>
                     <S.StepContent>
                         <S.StepName>{name}</S.StepName>
