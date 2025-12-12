3910b31c3e656eb35968404f9dca7b7af17240ba
Rafael Pereira <orafaeldev@gmail.com>
2025-12-12 18:12:37 -0300

    feat: adiciona hook useValidateName

---
+++ src/hooks/useValidateName.ts
@@ -0,0 +1,78 @@
+import { useEffect, useMemo, useState } from 'react'
+
+import { getOnlyLetters } from '@/utils/masks'
+import { usePersonalInfo } from '@/hooks/usePersonalInfo'
+
+export type OnValidateNameType = {
+    nameToValidate: string
+}
+
+export type InputErrorType = {
+    hasError: boolean
+    message?: string
+}
+
+export const useValidateName = () => {
+    const { info } = usePersonalInfo()
+
+    const [error, setError] = useState<InputErrorType>(<InputErrorType>{})
+    const [name, setName] = useState(info?.name || '')
+    const [canInitValidation, setCanInitValidation] = useState(
+        Boolean(info?.name)
+    )
+
+    const onStartValidateNameOnBlur = () => {
+        if (!canInitValidation) {
+            setCanInitValidation(true)
+        }
+    }
+
+    const onUpdateName = (nameToUpdate: string) => {
+        const clearName = nameToUpdate.replace(getOnlyLetters, '')
+
+        if (clearName.length > 2) {
+            onStartValidateNameOnBlur()
+        }
+
+        setName(clearName)
+    }
+
+    const handleNameInputState = useMemo(() => {
+        if (error.hasError) {
+            return 'error'
+        }
+
+        if (canInitValidation && !error.hasError) {
+            return 'success'
+        }
+
+        return 'invalidated'
+    }, [error, canInitValidation])
+
+    const onValidateName = ({ nameToValidate }: OnValidateNameType) => {
+        if (nameToValidate.length < 3) {
+            setError({
+                hasError: true,
+                message: 'Nome deve ter pelo menos 3 caracteres.',
+            })
+
+            return
+        }
+
+        setError({ hasError: false })
+    }
+
+    useEffect(() => {
+        if (canInitValidation) {
+            onValidateName({ nameToValidate: name })
+        }
+    }, [name, canInitValidation])
+
+    return {
+        name,
+        nameError: error,
+        nameState: handleNameInputState,
+        onUpdateName,
+        onStartValidateNameOnBlur,
+    }
+}
